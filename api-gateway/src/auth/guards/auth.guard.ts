52879eca225950b06f45a4d432feb7b763429a6a
Alexandre Soares <alexandressoares383@gmail.com>
2026-02-16 09:42:50 -0300

    feat: implements jwt auth guard

---
+++ api-gateway/src/auth/guards/auth.guard.ts
@@ -0,0 +1,34 @@
+
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
+
+const JwtAuthGuardExtends = AuthGuard('jwt');
+
+@Injectable()
+export class JwtAuthGuard extends JwtAuthGuardExtends {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>('isPublic', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
+    return super.canActivate(context);
+  }
+
+  handleRequest(err: any, user: any, _info: any) {
+    if (err || !user) {
+      throw err || new UnauthorizedException();
+    }
+    return user;
+  }
+}
